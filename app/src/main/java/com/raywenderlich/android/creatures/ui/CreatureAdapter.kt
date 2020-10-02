5bee24042585bc59e931781dd05dad20ea948a6d
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-02 11:45:29 -0500

    Finally got CreatureAdapter.kt to compile

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -7,7 +7,20 @@ import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
 
-class CreatureAdapter {
+class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(parent.inflate(R.layout.list_item_creature))
+    }
+
+    override fun getItemCount() = creatures.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
 
     class ViewHolder(v:View) : RecyclerView.ViewHolder(v) {}
+
+
 }
