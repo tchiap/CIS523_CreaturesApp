983bbba8de59ee98b17b5fee51421f9696a35c8b
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-02 11:52:11 -0500

    Got the creatures to list with their names in the RecyclerView

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.list_item_creature.view.*
 
 class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
@@ -16,11 +17,20 @@ class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adap
     override fun getItemCount() = creatures.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.bind(creatures[position])
     }
 
 
-    class ViewHolder(v:View) : RecyclerView.ViewHolder(v) {}
+    class ViewHolder(v:View) : RecyclerView.ViewHolder(v) {
 
+        private lateinit var creature: Creature
+
+        fun bind(creature: Creature) {
+            this.creature = creature
+            val context = itemView.context
+            itemView.creatureImage.setImageResource(context.resources.getIdentifier(creature.uri, null, context.packageName))
+            itemView.fullName.text = creature.fullName
+        }
+    }
 
 }
