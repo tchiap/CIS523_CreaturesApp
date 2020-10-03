3db7f20caaa5ea318ab342b1d94eaa52e363507b
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-03 12:29:00 -0500

    Completed Switching Between Span Sizes

--- app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
@@ -34,9 +34,7 @@ import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.model.CreatureStore
 import kotlinx.android.synthetic.main.fragment_all.*
@@ -46,10 +44,43 @@ class AllFragment : Fragment() {
 
   private val adapter = CreatureCardAdapter(CreatureStore.getCreatures().toMutableList())
 
+  private lateinit var layoutManager: StaggeredGridLayoutManager
+
   companion object {
     fun newInstance(): AllFragment {
       return AllFragment()
     }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setHasOptionsMenu(true)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    super.onCreateOptionsMenu(menu, inflater)
+    inflater.inflate(R.menu.menu_all, menu)
+  }
+
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    val id = item.itemId
+    when (id) {
+      R.id.action_span_1 ->
+      {
+        showListView()
+        return true
+      }
+      R.id.action_span_2 ->
+      {
+        showGridView()
+        return true
+      }
+    }
+
+
+    return super.onOptionsItemSelected(item)
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -59,8 +90,17 @@ class AllFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val layoutManager = StaggeredGridLayoutManager(2, GridLayoutManager.VERTICAL)
+    //val layoutManager = StaggeredGridLayoutManager(2, GridLayoutManager.VERTICAL)
+    layoutManager = StaggeredGridLayoutManager(2, GridLayoutManager.VERTICAL)
     creatureRecyclerView.layoutManager = layoutManager
     creatureRecyclerView.adapter = adapter
+  }
+
+  private fun showListView() {
+    layoutManager.spanCount = 1
+  }
+
+  private fun showGridView() {
+    layoutManager.spanCount = 2
   }
 }
